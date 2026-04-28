5ddd3c40dd5d54ad08ca45354566ebf1867f252f
Y M <90426808+yuka-with-data@users.noreply.github.com>
2026-04-28 11:57:47 -0700

    Initial commit

---
+++ config.py
@@ -0,0 +1,11 @@
+""" 
+Transit backend config toggle
+
+USE_EXPERIMENTAL controls which data source is used for route calculations.
+- True: Uses experimental scraping-based implementation
+        (temporary, unstable, not production-safe)
+- False: Uses structured / stable data source implementation
+        (e.g., ODPT, publicly available, open sourced data)
+
+ """
+USE_EXPERIMENTAL = True
